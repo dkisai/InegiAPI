213f1602c032c2af635c112b2a3cf38b832ea7c5
dkisai <diego.kisai@live.com>
2022-02-18 13:44:10 -0600

    Listo

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import pandas as pd
+import folium
+from folium import plugins
+
+# url con token
+# Ejemplo: todos los bares en celaya en un radio de 5km del centro
+token = "59607fb5-9eeb-d10b-ca13-c4986cfb749a"
+rango = "5000"
+latit = "20.522188"
+longi = "-100.814037"
+url = "https://www.inegi.org.mx/app/api/denue/v1/consulta/Buscar/bar/"+latit+","+longi+"/"+rango+"/"+token
+# Respuesta de la consulta a la API
+respuesta = requests.get(url)
+respuesta = respuesta.text
+data = json.loads(respuesta)
+df = pd.DataFrame(data)
+#Seleccionamos solo la latitud y longitud
+df = df[["Latitud", "Longitud"]]
+#Convertimos nuestra df a un array
+df = df.values
+#Mapa en Folium
+mapa = folium.Map(
+    location = [latit,longi],
+    titles = 'OpenStreetMap',
+    zoom_start=13
+)
+#Heatmap
+mapa.add_child(plugins.HeatMap(df, radius=20))
+mapa.save("mapa.html")
